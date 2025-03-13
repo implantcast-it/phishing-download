ab63aa0991d6706700cdd736358715a5558bf897
Edwin Rheindt <e.rheindt@implantcast.de>
2025-03-13 11:11:38 +0100

    Added website content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <div className='place-items-center grid'>
-      <a></a>
+    <div className='place-items-center grid w-full h-screen'>
+      <a
+        className='block bg-emerald-700 shadow p-3 rounded-md font-medium text-white text-lg'
+        href='./meeting.exe'
+      >
+        Download Meeting Software
+      </a>
     </div>
   );
 }
